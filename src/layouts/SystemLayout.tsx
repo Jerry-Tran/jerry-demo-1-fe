9b024b4c01bedc90f475b7d64989d98ba112bd44
Jerry-Tran <jerry.tran.goldenowl@gmail.com>
2024-09-27 15:41:35 +0700

    set up layout admin

--- src/layouts/SystemLayout.tsx
+++ src/layouts/SystemLayout.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
 
-export const SystemLayout = () => {
+import { useSelector } from 'react-redux'
+
+import { Outlet, useNavigate } from 'react-router-dom'
+
+import { Layout } from 'antd'
+
+import { RootState } from '@/store'
+
+import { SystemHeader } from './admin/Header'
+
+import { Sidebar } from './admin/Sidebar'
+
+const { Content } = Layout
+
+const contentStyle: React.CSSProperties = {
+  backgroundColor: '#fafafa'
+}
+
+const layoutStyle = {
+  overflow: 'hidden',
+  height: '100vh'
+}
+export function SystemLayout() {
+  const navigate = useNavigate()
+  const { currentUser } = useSelector((state: RootState) => state.auth)
+  useEffect(() => {
+    if (!currentUser || currentUser?.role !== 'admin') {
+      navigate('/admin/login')
+    }
+  }, [navigate, currentUser])
   return (
-    <div>
-      System layout
-      <Outlet />
-    </div>
+    <Layout style={layoutStyle}>
+      <Sidebar />
+      <Layout>
+        <SystemHeader />
+        <Content style={contentStyle}>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
